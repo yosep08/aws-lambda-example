92838dfaa21fdcbd428534901db1f049c48f460b
tresnax <tresna.widiyaman@gmail.com>
2024-08-01 14:49:35 +0700

    add dependency

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-const AWS = require('aws-sdk');
-const dynamo = new AWS.DynamoDB.DocumentClient();
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { ScanCommand } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient();
 
 exports.handler = async (event) => {
     const params = {
-        TableName: 'PeopleTable'
+        TableName: 'PeopleTable',
     };
 
     try {
-        const data = await dynamo.scan(params).promise();
-        const response = {
+        const data = await client.send(new ScanCommand(params));
+        return {
             statusCode: 200,
-            body: JSON.stringify(data.Items)
+            body: JSON.stringify(data),
         };
-        return response;
     } catch (error) {
         console.error(error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Could not fetch data' })
+            body: JSON.stringify({ error: 'Could not fetch data' }),
         };
     }
 };
